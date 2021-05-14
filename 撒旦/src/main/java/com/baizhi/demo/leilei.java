b8facbf5cd505365ba7de84d428099f2a5f1543d
SunnyOmO <80448516+SunnyOmO@users.noreply.github.com>
2021-05-14 11:48:47 +0800

    Create leilei.java

--- 撒旦/src/main/java/com/baizhi/demo/leilei.java
+++ 撒旦/src/main/java/com/baizhi/demo/leilei.java
@@ -5,5 +5,5 @@ import java.io.Serializable;
 public class leilei implements Serializable {
     private Integer ud;
     private String id;
-    private Boolean set;
+    private Boolean set;//你好吗
 }

cf299668e761568cacc83a699b59374970951d58
Lenovo <973165813@qq.com>
2021-05-14 11:54:16 +0800

    创建了个实体类,添加了日期

--- 撒旦/src/main/java/com/baizhi/demo/leilei.java
+++ 撒旦/src/main/java/com/baizhi/demo/leilei.java
@@ -1,9 +1,11 @@
 package com.baizhi.demo;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class leilei implements Serializable {
     private Integer ud;
     private String id;
     private Boolean set;//你好吗
+    private Date Ssd;
 }
